4e9b4f06945f1f13ff370735edda3a551a8262e3
cansuozdizlekli <101900120+cansuozdizlekli@users.noreply.github.com>
2024-05-08 12:46:15 +0300

    Initial commit on main branch

---
+++ MovieSwiftUI/MovieSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSwiftUIApp.swift
+//  MovieSwiftUI
+//
+//  Created by Cansu Özdizlekli on 5/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
