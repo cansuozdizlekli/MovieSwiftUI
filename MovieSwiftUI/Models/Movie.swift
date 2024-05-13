36f779c81ed017fa5bee89e5054c39dbbac532fa
cansuozdizlekli <101900120+cansuozdizlekli@users.noreply.github.com>
2024-05-13 14:16:04 +0200

    added movie & movie detail models

---
+++ MovieSwiftUI/Models/Movie.swift
@@ -0,0 +1,49 @@
+//
+//  Movie.swift
+//  MovieSwiftUI
+//
+//  Created by Cansu Özdizlekli on 5/8/24.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let page: Int?
+    let results: [Results]?
+    let totalPages, totalResults: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Result
+struct Results: Codable , Identifiable {
+    let adult: Bool?
+    let backdropPath: String?
+    let genreIDS: [Int]?
+    let id: Int
+    let originalLanguage, originalTitle, overview: String?
+    let popularity: Double?
+    let posterPath, releaseDate, title: String?
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
